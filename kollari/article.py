4566756312260aa0cb37f51db2ca0de50682a03a
Russell <russell@vort.org>
2015-10-15 00:32:38 -0700

    stubbed out some of the features...

---
+++ kollari/article.py
@@ -0,0 +1,13 @@
+import json
+
+class article :
+    def __init__( self ) :
+        self.name = ''
+        self.sections = []
+
+    def read_notebook( self, notebook ) :
+        nb = json.read( notebook )
+        for cell in nb.items()[2][1][0]['cells'] :
+            self.section = { 'type'     : cell['cell_type'],
+                             'language' : cell['language'] }
+        
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='kollari',
+      version='0.1',
+      description='A tool for publishing and operating a scholarly journal',
+      scripts=['scripts/kollari'],
+      url='http://github.com/ryneches/kollari',
+      author='Russell Neches',
+      author_email='ryneches@ucdavis.edu',
+      license='BSD',
+      packages=['kollari'],
+      zip_safe=False)
